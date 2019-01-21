0538d1717407b4de7dee067aae88b7e2f2070377
axelcesi <38346473+axelcesi@users.noreply.github.com>
2019-01-21 16:02:39 +0100

    site en construction
    hello

--- website/store/index.php
+++ website/store/index.php
@@ -5,6 +5,10 @@
         <meta charset="utf-8" />
 
         <link rel="stylesheet" href="../assets/css/store.css">
+        <link rel="stylesheet" href="../assets/js/store.js">
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 
         <title>Mon super site</title>
         <?php $PAGE="cart"; ?>
@@ -15,7 +19,6 @@
 
 
     <div id="menu">Menu
-
         <div class="topnav">
 
             <div class="search-container">
@@ -70,29 +73,234 @@
 
     <div id="contenu" >
 
+
         <div id="contenu1">
-            <p>je suis le titre de l'article</p>
-        </br>
-            <p>je suis l'image de l'article</p>
-        </br>
-            <p>je suis la quantite</p>
-        </br>
+
+            <div id="contenu10">
+
+                <div id="contenu100">
+                    <p>je suis le titre de l'article</p>
+                    </br>
+                </div>
+
+                <div id="contenu101">
+                <img
+                    src="../assets/img/t-shirt.jpg" 
+                    alt="t-shirt gris du bde"
+                    height="100%" 
+                    width="100%" 
+                />
+                     </br>
+                </div>
+
+                <div id="contenu102">
+                         <p>
+                        </p>
+                            <div class="input-group">
+                            <span class="input-group-btn">
+                                <button type="button" class="btn btn-primary btn-number"  data-type="minus" data-field="quant[2]">
+                                    <span class="glyphicon glyphicon-minus"></span>
+                                </button>
+                            </span>
+                            <input type="number" name="quant[2]" class="form-control input-number" value="0" min="1" max="100">
+                            <span class="input-group-btn">
+                                <button type="button" class="btn btn-primary btn-number" data-type="plus" data-field="quant[2]">
+                                    <span class="glyphicon glyphicon-plus"></span>
+                                </button>
+                            </span>
+                        </div>
+                        <p></p>
+                    </br>
+                </div>
+                <div id="contenu103">
+                    <div id="bouton_bde">
+                         <button type="button" class="btn btn-danger btn-circle"><i class="glyphicon glyphicon-remove"></i></button>
+                        <button type="button" class="btn btn-warning btn-circle"><i class="glyphicon glyphicon-cog"></i></button>
+                    </div>
+                </div>
+
+            </div>
+
+            <div id="contenu20">
+
+                <div id="contenu200">
+                    <p>je suis le titre de l'article2</p>
+                    </br>
+                </div> 
+
+                <div id="contenu201">
+                    <img
+                        src="../assets/img/t-shirt.jpg" 
+                        alt="t-shirt gris du bde"
+                        height="100%" 
+                        width="100%" 
+                    />
+                     </br>
+                </div>
+
+                <div id="contenu202">
+                        <p>
+                        </p>
+                            <div class="input-group">
+                            <span class="input-group-btn">
+                                <button type="button" class="btn btn-primary btn-number"  data-type="minus" data-field="quant[2]">
+                                    <span class="glyphicon glyphicon-minus"></span>
+                                </button>
+                            </span>
+                            <input type="number" name="quant[2]" class="form-control input-number" value="0" min="1" max="100">
+                            <span class="input-group-btn">
+                                <button type="button" class="btn btn-primary btn-number" data-type="plus" data-field="quant[2]">
+                                    <span class="glyphicon glyphicon-plus"></span>
+                                </button>
+                            </span>
+                        </div>
+                        <p></p>
+                    </br>
+                </div>
+                <div id="contenu203">
+                    <div id="bouton_bde">
+                         <button type="button" class="btn btn-danger btn-circle"><i class="glyphicon glyphicon-remove"></i></button>
+                        <button type="button" class="btn btn-warning btn-circle"><i class="glyphicon glyphicon-cog"></i></button>
+                    </div>
+                </div>
+
+            </div>
+
+            <div id="contenu30">
+
+                <div id="contenu300">
+                    <p>je suis le titre de l'article3</p>
+                    </br>
+                </div>
+
+                <div id="contenu301">
+                    <img
+                        src="../assets/img/t-shirt.jpg" 
+                        alt="t-shirt gris du bde"
+                        height="100%" 
+                        width="100%" 
+                    />
+                    </br>
+                </div>
+
+                <div id="contenu302">
+                         <p>
+                        </p>
+                            <div class="input-group">
+                            <span class="input-group-btn">
+                                <button type="button" class="btn btn-primary btn-number"  data-type="minus" data-field="quant[2]">
+                                    <span class="glyphicon glyphicon-minus"></span>
+                                </button>
+                            </span>
+                            <input type="number" name="quant[2]" class="form-control input-number" value="0" min="1" max="100">
+                            <span class="input-group-btn">
+                                <button type="button" class="btn btn-primary btn-number" data-type="plus" data-field="quant[2]">
+                                    <span class="glyphicon glyphicon-plus"></span>
+                                </button>
+                            </span>
+                        </div>
+                        <p></p>
+                    </br>
+                </div>
+                <div id="contenu303">
+                    <div id="bouton_bde">
+                         <button type="button" class="btn btn-danger btn-circle"><i class="glyphicon glyphicon-remove"></i></button>
+                        <button type="button" class="btn btn-warning btn-circle"><i class="glyphicon glyphicon-cog"></i></button>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
+
         <div id="contenu2">
-            <p>je suis le titre de l'article2</p>
-        </br>
-            <p>je suis l'image de l'articl2</p>
-        </br>
-            <p>je suis la quantite2</p>
-        </br>
-        </div>
-        <div id="contenu3">
-            <p>je suis le titre de l'article3</p>
-        </br>
-            <p>je suis l'image de l'article3</p>
-        </br>
-            <p>je suis la quantite3</p>
-        </br>
+
+            <div id="contenu40">
+
+                <div id="contenu400">
+                    <p>je suis le titre de l'article4</p>
+                    </br>
+                </div>
+
+                <div id="contenu401">
+                    <img
+                        src="../assets/img/t-shirt.jpg" 
+                        alt="t-shirt gris du bde"
+                        height="100%" 
+                        width="100%" 
+                    />
+                    </br>   
+                </div>
+
+                <div id="contenu402">
+                <p>
+                <p>
+                        </p>
+                            <div class="input-group">
+                            <span class="input-group-btn">
+                                <button type="button" class="btn btn-primary btn-number"  data-type="minus" data-field="quant[2]">
+                                    <span class="glyphicon glyphicon-minus"></span>
+                                </button>
+                            </span>
+                            <input type="number" name="quant[2]" class="form-control input-number" value="0" min="1" max="100">
+                            <span class="input-group-btn">
+                                <button type="button" class="btn btn-primary btn-number" data-type="plus" data-field="quant[2]">
+                                    <span class="glyphicon glyphicon-plus"></span>
+                                </button>
+                            </span>
+                        </div>
+                        <p></p>
+                    </br>
+                </div>
+                <div id="contenu403">
+                    <div id="bouton_bde">
+                         <button type="button" class="btn btn-danger btn-circle"><i class="glyphicon glyphicon-remove"></i></button>
+                        <button type="button" class="btn btn-warning btn-circle"><i class="glyphicon glyphicon-cog"></i></button>
+                    </div>
+                </div>
+
+            </div>
+
+            <div id="contenu50">
+                <div id="contenu500">
+                     <p>je suis le titre de l'article5</p>
+                    </br>
+                </div>
+                <div id="contenu501">
+                    <img
+                        src="../assets/img/t-shirt.jpg" 
+                        alt="t-shirt gris du bde"
+                        height="100%" 
+                        width="100%" 
+                    />
+                     </br>
+                </div>
+                <div id="contenu502">
+                        <p>
+                        </p>
+                            <div class="input-group">
+                            <span class="input-group-btn">
+                                <button type="button" class="btn btn-primary btn-number"  data-type="minus" data-field="quant[2]">
+                                    <span class="glyphicon glyphicon-minus"></span>
+                                </button>
+                            </span>
+                            <input type="number" name="quant[2]" class="form-control input-number" value="0" min="1" max="100">
+                            <span class="input-group-btn">
+                                <button type="button" class="btn btn-primary btn-number" data-type="plus" data-field="quant[2]">
+                                    <span class="glyphicon glyphicon-plus"></span>
+                                </button>
+                            </span>
+                        </div>
+                        <p></p>
+                     </br>
+                </div>
+                <div id="contenu503">
+                    <div id="bouton_bde">
+                         <button type="button" class="btn btn-danger btn-circle"><i class="glyphicon glyphicon-remove"></i></button>
+                        <button type="button" class="btn btn-warning btn-circle"><i class="glyphicon glyphicon-cog"></i></button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -119,3 +327,4 @@
 
 
 </html>
+
