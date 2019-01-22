b898f3e5b074fd09f6177012cd92b925a6904cd6
Snargol <anatole.couasnon@orange.fr>
2019-01-22 10:19:28 +0100

    créer une idée front et visualiser front

---
+++ website/idea_box/AccueilBoxIdea.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+    </head>
+
+    <body class="body">
+        <?php $PAGE = 'AccueilBoxIdea'; ?>
+        <?php include("../common/header.php"); ?>
+        <?php include("../idea_box/BandeauSubmitIdea.php"); ?>
+        <?php include("../idea_box/BoxIdea.php"); ?>
+        <hr class = "separator">
+        <?php include("../idea_box/BoxIdea.php"); ?>
+        <hr class = "separator">
+        <?php include("../idea_box/BoxIdea.php"); ?>
+<!--        --><?php /*include("../idea_box/AddIdea.php")*/?>
+        <?php include("../common/footer.php"); ?>
+
+    </body>
+
+</html>
+
+<a class="btn btn-primary" href="#"><i class="fas fa-times"></i></a>
+<a class="btn btn-primary" href="#"><i class="fas fa-cog"></i></a>
