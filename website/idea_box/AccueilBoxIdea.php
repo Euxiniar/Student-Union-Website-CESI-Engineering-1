76976be2df30194a891309e42811c17d9d0e575f
Snargol <anatole.couasnon@orange.fr>
2019-01-23 13:41:26 +0100

    afficher idées

--- website/idea_box/AccueilBoxIdea.php
+++ website/idea_box/AccueilBoxIdea.php
@@ -6,12 +6,21 @@
         <?php $PAGE = 'AccueilBoxIdea'; ?>
         <?php include("../common/header.php"); ?>
         <?php include("../idea_box/BandeauSubmitIdea.php"); ?>
-        <?php include("../idea_box/BoxIdea.php"); ?>
-        <hr class = "separator">
-        <?php include("../idea_box/BoxIdea.php"); ?>
-        <hr class = "separator">
-        <?php include("../idea_box/BoxIdea.php"); ?>
-<!--        --><?php /*include("../idea_box/AddIdea.php")*/?>
+
+        <?php
+        include("../scripts/setConnexionLocalBDD.php");
+        $user = $local_bdd->query('call orleans_bde.spl_evenements_idea();');
+
+        while($datasEvent = $user->fetch()){
+            include("../idea_box/BoxIdea.php");
+            echo '<hr class = "separator">';
+        }
+
+        $user->closeCursor();
+
+        ?>
+
+
         <?php include("../common/footer.php"); ?>
 
     </body>
