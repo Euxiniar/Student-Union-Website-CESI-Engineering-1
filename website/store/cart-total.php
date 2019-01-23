06ce8be0df4cf283ede872ad3f04a975cd1193a9
Joël Didier <33155385+Studisys@users.noreply.github.com>
2019-01-24 00:16:27 +0100

    Updated Cart Listing
    Items are now displayed according to database.

---
+++ website/store/cart-total.php
@@ -0,0 +1,18 @@
+<tfoot>
+                <tr>
+                    <td>
+                        <button type="button" class="btn btn-info"><span class="glyphicon glyphicon-chevron-left"></span> Continuer les achats</button>
+                        <button type="button" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Vider le panier</button>
+                    </td>
+
+                    <td colspan="2" class="hidden-xs"></td>
+                    <td class="hidden-xs text-center">Prix Total : <span id="cart-total-cost"><?php echo $datasEvent['Cout_total']?>€</span>
+                    
+                    </td>
+                    <td>
+                        <button type="button" class="btn btn-success">
+                            <span class="glyphicon glyphicon-shopping-cart"></span> Commander
+                        </button>
+                    </td>
+                </tr>
+            </tfoot>
