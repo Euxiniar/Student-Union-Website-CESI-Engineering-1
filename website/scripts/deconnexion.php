8efb0e6d96c5b5a4274a05ff68d27db6eb4ba497
Snargol <anatole.couasnon@orange.fr>
2019-01-23 21:43:52 +0100

    deconnexion OK (mais moche)

--- website/scripts/deconnexion.php
+++ website/scripts/deconnexion.php
@@ -1,9 +1,21 @@
 <?php
 
+ include("../common/header.php");
+
+
 if (isset($_SESSION)) {
     session_destroy();
     $_SESSION = null;
 }
-echo $_SESSION;
 
+if (isset($_SESSION)) {
+    echo '<h1>Vous êtes connectés</h1>';
+}
+else {
+    echo '<h1>vous avez été déconnecté</h1>';
+}
+
+echo '<p><a href ="../disconnected/home.php">retour à l\'acceuil</a></p>';
+
+include("../common/footer.php");
 ?>
