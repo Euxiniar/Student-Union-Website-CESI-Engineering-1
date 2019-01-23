83de3c93ab39d89fe30a78a40d35dfba3e9dd8ac
Euxiniar <charles.ago37@gmail.com>
2019-01-23 20:50:39 +0100

    add home connected disconnected navbar

--- website/disconnected/home.php
+++ website/disconnected/home.php
@@ -3,9 +3,6 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>		 -->
 		<title>BDE CESI Exia</title>
 		<?php $PAGE = "home" ?>
 	</head>
@@ -18,10 +15,10 @@
 		<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-			<h1 class="display-3 text-white text-center">Bienvenue sur le site du BDE</h1>
-			<div class="row">
-				<div class="col-md-12 text-center"><a class="btn btn-primary mr-3" href="../disconnected/connexion.php#tologin" >Connexion</a><a class="btn btn-primary" href="../disconnected/connexion.php#toregister">Inscription</a></div>
-			</div>
+				<h1 class="display-3 text-white text-center py-auto">Bienvenue sur le site du BDE</h1>
+				<div class="row">
+					<div class="col-md-12 text-center py-auto"><a class="btn btn-primary mr-3" href="../disconnected/connexion.php#tologin" >Connexion</a><a class="btn btn-primary" href="../disconnected/connexion.php#toregister">Inscription</a></div>
+				</div>
 			</div>
 		</div>
 		</div>
