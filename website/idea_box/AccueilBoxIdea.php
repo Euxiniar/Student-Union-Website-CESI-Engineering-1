9106cd9d7b7ab7a9e317a1014557a43fff1cc0d8
Snargol <anatole.couasnon@orange.fr>
2019-01-24 14:39:59 +0100

    suppr idee Back OK

--- website/idea_box/AccueilBoxIdea.php
+++ website/idea_box/AccueilBoxIdea.php
@@ -9,13 +9,31 @@
 
         <?php
         include("../scripts/setConnexionLocalBDD.php");
+
+        if (isset($_POST['id'])) {
+            if (isset($_POST['button-suppr'])) {
+                $local_bdd->query('call orleans_bde.spd_evenement_by_id(' . $_POST['id'] . ');');
+/*                echo '<meta http-equiv="refresh" content="0">';*/
+            }
+            $_POST['id'] =null;
+            $_POST['button-suppr'] =null;
+        }
+
         $user = $local_bdd->query('call orleans_bde.spl_evenements_idea();');
+        $id_events = array();
+
 
         while($datasEvent = $user->fetch()){
+/*            include("../idea_box/BoxIdea.php");
+            echo '<hr class = "separator">';*/
+            $id_events[] = $datasEvent['Id_evenement'];
+        }
+        $user->closeCursor();
+
+        foreach($id_events as $id_event) {
             include("../idea_box/BoxIdea.php");
             echo '<hr class = "separator">';
         }
-        $user->closeCursor();
 
         ?>
         <?php include("../common/footer.php"); ?>
