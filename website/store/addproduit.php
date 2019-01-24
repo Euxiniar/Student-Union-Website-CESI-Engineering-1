86d9fc31ea7800c47ae2be8310382375d86433a3
axelcesi <38346473+axelcesi@users.noreply.github.com>
2019-01-24 14:51:10 +0100

    changement page
    addproduit

--- website/store/addproduit.php
+++ website/store/addproduit.php
@@ -8,70 +8,49 @@
 </head>
 <body>
 <?php include("../common/header.php") ?>
-<div class="grid-container">
+<div class = "IdeaBox p-2 pr-4 mt-4">
+    <form class="container-fluid" method="post"  autocomplete="on" enctype="multipart/form-data">
+        <div class="row">
+            <div class="col-md-12"> <!--Titre-->
+                <t2> Création d'un article</t2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6"> <!--Photo-->
 
-
-
-
-  <div class="Photo_couverture">
-  <input id="filebutton" name="filebutton" class="input-file" type="file" accept="image/png, image/jpeg">>
-
-
-  </div>
-
-
-
-
-  <div class="Nom_produit">
-
-        <TEXTAREA rows=2 COLS=100> 
-        Nom du produit
-        </TEXTAREA>
-
-
-  </div>
-
-
-
-
-  <div class="Description_produit">
-
-        <TEXTAREA rows=4 COLS=100> 
-        Description du produit 
-        </TEXTAREA>
-
-  </div>
-
-
-
-
-  <div class="Prix_produit">
-
-        <TEXTAREA rows=1 COLS=40> 
-        Prix €
-        </TEXTAREA> 
-
-  </div>
-
-
-
-
-
-  <div class="Annuler_produit" id="bouton_produit" >
-
-        <button type="button" class="btn btn-danger">
-                <a id="a"href="index.php">Annuler</a>
-            </button>
-
-  </div>
-
-
-
-  <div class="Ajouter_produit" id="bouton_produit">
-
-        <button type="button" class="btn btn-success">Ajouter</button>
-
-  </div>
+            </div> <!--Photo-->
+            <div class="col-md-6"> <!--Nom Article-->
+                <t3>Nom de l'article</t3><br/>
+                <input type="text"  name="titre" placeholder="Nom de l'article" class ="mb-2" maxlength="50" required="required" autofocus ><br/>
+                <br/>
+                <t3>Description de l'article</t3><br/>
+                <textarea class="form-control" id="description" name="description" required="required" maxlength="2000"></textarea>
+            </div> 
+        </div>
+        <div class="row">
+            <div class="col-md-6"> <!--Choisir un fichier-->
+                <t3 class ="pl-3 ">Image | max 1 Mo </t3><br/>
+                <input type="hidden" name="MAX_FILE_SIZE" value="1048576" accept="image/png, image/jpeg" /> <!--Mac 1Mo-->
+                <input id="filebutton" name="filebutton" class="input-file pl-3" type="file" required="required" accept="image/png, image/jpeg" >
+            </div> <!--Choisir un fichier-->
+            <div class="col-md-3"> <!--cout-->
+                <t3>Cout de l'article </t3>
+                <br/>
+                <input type="number" name="quantity" placeholder ="0€" min="0" max="999">
+            </div> <!--cout-->
+        </div>
+        <div class="row">
+            <div class="col-md-12 m-3 "> 
+            <div class="submit">
+                    <button id="submit" name="submit" class="btn btn-danger m-3">Annuler</button>
+                    <button id="submit" name="submit" class="btn btn-success m-3">Ajouter</button>
+                </div>
+            </div> 
+        </div>
+    </form>
 </div>
+
+
+<?php include("../common/footer.php"); ?>
 </body>
 </html>
