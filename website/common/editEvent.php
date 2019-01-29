4b125408a735b5ed217ce73ec1ec3e112411830c
Snargol <anatole.couasnon@orange.fr>
2019-01-29 11:08:20 +0100

    edit event

--- website/common/editEvent.php
+++ website/common/editEvent.php
@@ -4,65 +4,72 @@
 
 </head>
 
-<body class ="common-background-blue">
+<body class="common-background-blue">
 <?php
-$PAGE = 'editIdea';
+$PAGE = 'Ajouter une idée';
 include("../common/header.php");
 include("../scripts/setConnexionLocalBDD.php");
+if(!isset($_SESSION)){
+session_start();
+}
+if (isset($_POST['id'])){
+$_SESSION['id_event'] = $_POST['id'];
+}
 
-if (isset($_POST['id'])) {
-    $evenement = $local_bdd->query('call orleans_bde.sps_evenement('. $_POST['id'].');');
+if (isset($_SESSION['id_event'])) {
+    $evenement = $local_bdd->query('call orleans_bde.sps_evenement(' . $_SESSION['id_event'] . ');');
     $datasEvenement = $evenement->fetch();
-    $evenement->closeCursor();
+    $evenement->closeCursor(); ?>
 
-    ?>
     <div class = "IdeaBox p-2 pr-4 mt-4">
         <form class="container-fluid" method="post"  autocomplete="on" enctype="multipart/form-data">
             <div class="row">
                 <div class="col-md-12"> <!--Titre-->
-                    <span class="common-t3 mb-5"> Modification d'une idée d'évènement</span>
+                    <span class="common-t2"> Création d'une idée d'évènement</span>
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-6"> <!--Photo-->
+                <div class="col-md-4"> <!--Photo-->
                     <img alt="Photo de couverture de l'évènement" src="<?php echo $datasEvenement['URL_photo'] ?>"  class="ml-3 common-couverture">
                 </div> <!--Photo-->
-                <div class="col-md-6"> <!--Titre date récurrence-->
-                    <t3>Edition de l'idée</t3><br/>
-                    <input type="text"  name="title" placeholder="Titre de l'idée" class ="mb-2" maxlength="50" required="required" value ="<?php echo $datasEvenement['Titre']; ?>" autofocus ><br/>
-                    <?php $evenement->closeCursor(); ?>
-                    <t3>Date de l'évènement</t3><br/>
-                    <input type="date" name="date" class ="mb-2" required="required" value ="<?php echo  $datasEvenement['Date_evenement']; ?>"><br/>
-                    <t3>Récurrence de l'évènement</t3><br/>
-                    <select id="selectbasic" name="recurrence" class="form-control mb-2" required="required" value ="<?php echo $local_bdd->query('call orleans_bde.sps_recurrence('.$datasEvenement['Id_reccurrence'].');')->fetch();  ?>"><br/>
+                <div class="col-md-4"> <!--Titre date récurrence-->
+                    <span class="common-t3">Titre de l'idée</span><br/>
+                    <input class="form-control" type="text" name="title" placeholder="Titre de l'idée" value ="<?php echo $datasEvenement['Titre']; ?>" required autofocus>
+                    <span class="common-t3">Récurrence de l'évènement</span><br/>
+                    <select id="selectbasic" name="recurrence" required class="form-control mb-2"><br/>
                         <option value="4" name="recurrence">Aucune récurrence</option>
                         <option value="1" name="recurrence">1 semaine</option>
                         <option value="2" name="recurrence">1 mois</option>
                         <option value="3" name="recurrence">1 ans</option>
                     </select>
-                    <input type="hidden" name="id_evenement" value="<?php echo $datasEvenement['Id_evenement'] ?>"/>
-                </div> <!--Titre date récurrence id-->
+                </div> <!--Titre date récurrence-->
+                <div class="col-md-4">
+                    <span class="common-t3">Heure de l'évènement</span><br/>
+                    <input class="form-control" type="time" value="12:00:00" name="time">
+                    <span class="common-t3 mt-2" >Date de l'évènement</span><br/>
+                    <input class="form-control" type="date" name="date" value ="<?php echo  $datasEvenement['Date_evenement']; ?>" id="example-date-input" required><br/>
+                </div>
             </div>
             <div class="row">
-                <div class="col-md-6"> <!--Choisir un fichier-->
-                    <t3 class ="pl-3 ">Image | max 50 Mo </t3><br/>
-                    <input type="hidden" name="MAX_FILE_SIZE" value="52428800" /> <!--Max 50Mo-->
+                <div class="col-md-4"> <!--Choisir un fichier-->
+                    <span class ="pl-3 common-t3">Image | max 50 Mo </span><br/>
+                    <input type="hidden" name="" value="52428800" /> <!--Max 50Mo-->
                     <input id="filebutton" name="filebutton" class="input-file pl-3" type="file" accept="image/png, image/jpeg, image/jpg, image/gif"  >
                 </div> <!--Choisir un fichier-->
-                <div class="col-md-3"> <!--cout-->
-                    <t3>Cout prévisionnel </t3>
+                <div class="col-md-4"> <!--cout-->
+                    <span class="common-t3">Cout prévisionnel </span>
                     <br/>
-                    <input type="number" name="quantity" placeholder ="0€" value ="<?php echo $datasEvenement['Cout']; ?>" min="0" max="999">
+                    <input class="form-control" type="number" value ="<?php echo $datasEvenement['Cout']; ?>" name="quantity" placeholder ="0€" min="0" max="999">
                 </div> <!--cout-->
-                <div class="col-md-3 m"> <!--adresse-->
-                    <t3>Adresse de l'évènement</t3>
+                <div class="col-md-4 m"> <!--adresse-->
+                    <span class="common-t3">Adresse de l'évènement</span>
                     <br/>
-                    <input type="text" id="adr" name="address" placeholder="1 allé du titane" value ="<?php echo $datasEvenement['Lieu']; ?>" maxlength="50">
+                    <input type = "text" class="form-control" id="adr" name="address" value ="<?php echo $datasEvenement['Lieu']; ?>" maxlength="50">
                 </div><!--adresse-->
             </div>
             <div class="row">
                 <div class="col-md-12 m-3 "> <!--Description-->
-                    <t3>Description</t3>
+                    <span class="common-t3">Description</span>
                     <br/>
                     <textarea class="form-control" id="description" name="description" required="required" maxlength="2000"><?php echo $datasEvenement['Description']; ?></textarea>
                 </div> <!--Description-->
@@ -70,25 +77,35 @@ if (isset($_POST['id'])) {
             <div class="row">
                 <div class="col-md-12"> <!--Boutton submit-->
                     <div class="submit">
-                        <form method = "post" action ="editIdea.php">
-                            <a href="../idea_box/AccueilBoxIdea.php" class="btn btn-danger m-3" >Annuler</a>
-                            <button id="submit" name="submit" class="btn btn-success m-3">Modifier l'idée</button>
-                        </form>
+                        <input type="hidden" name="id_status_date" value="1"/>
+                        <a href="../idea_box/AccueilBoxIdea.php" id="submit" name="submit" class="btn btn-danger m-3" >Annuler</a>
+                        <button id="submit" name="submit" class="btn btn-success m-3">Modifier l'évènement</button>
                     </div>
                 </div> <!--Boutton submit-->
             </div>
         </form>
     </div>
-    <?php
-}
-else {
-    if(isset($_POST['submit'])) {
-        include("../scripts/editEvent.php");
-    }
-    echo '<meta http-equiv="refresh" content="0; URL=../idea_box/AccueilBoxIdea.php">';
-}
-include("../common/footer.php");
-?>
+<?php
+}?>
+
+<?php if(isset($_POST['submit'])) {
+    include("../scripts/editEvent.php");
+}?>
+
+<?php include("../common/footer.php"); ?>
 </body>
 
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
--- website/scripts/editEvent.php
+++ website/scripts/editEvent.php
@@ -1,99 +1,99 @@
 <?php
 include("../scripts/setConnexionLocalBDD.php");
 
+
+$event = $local_bdd->query('call orleans_bde.sps_evenement('.$_SESSION['id_event'].');');
+$DatasEvent = $event->fetch();
+$event->closeCursor();
+
 $creationDate = date("Y-m-d");
 
 /*Vérification du fichier upload*/
-
 $erreur = 'pas derreur';
 if ($_FILES['filebutton']['error'] > 0)
     $erreur = "Erreur lors du transfert". '<br>';
-/*echo $erreur;*/
 if ($_FILES['filebutton']['size'] > 52428800) /*50Mo*/
     $erreur = "Le fichier est trop gros". '<br>';
-/*echo $erreur;*/
 
 $extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png' );
 $extension_upload = strtolower(  substr(  strrchr($_FILES['filebutton']['name'], '.')  ,1)  );
 if ( !in_array($extension_upload,$extensions_valides) )
     $erreur = "L'extension n'est pas valide". '<br>';
 
-/*echo $erreur;*/
-$hours = date("H-i-s");
-$id = $_POST['id_evenement'];
-if ($_FILES['filebutton']['name'] != 0){
+if (mb_strlen($_FILES['filebutton']['name']) > 0){
+    $hours = date("H-i-s");
     $nomNewFichier = htmlspecialchars($_SESSION['id'].'-'.$creationDate.'-' . $hours . '-' . $_FILES['filebutton']['name']);
-    /*echo $nomNewFichier. '<br>';;*/
-    $url = '../assets/img/couvertures/'.$nomNewFichier;
+    $url = '../uploads/cover/'.$nomNewFichier;
+
+    /*if the file is different, we create a new file*/
+    if ($DatasEvent['URL_photo'] != $url){
+        $resultat = move_uploaded_file($_FILES['filebutton']['tmp_name'],$url);
+        if (!$resultat)
+            $erreur = "Erreur de transfert". '<br>';
+    }
 }
 else {
-    $event = $local_bdd->query('call orleans_bde.sps_evenement('.$id.');');
-    $dataEvent = $event->fetch();
-    $event->closeCursor();
-    $url = $dataEvent['URL_photo'];
+    $url = $DatasEvent['URL_photo'];
 }
 
-
-
-$resultat = move_uploaded_file($_FILES['filebutton']['tmp_name'],$url);
-/*if ($resultat)
-    echo "Transfert réussi". '<br>';
-else {
-    $erreur = "Erreur de transfert". '<br>';
-}*/
 if ($erreur = 'pas derreur') {
     /*Fin de la vérification*/
     $replace = "\\\'";
 
-    /*id_event*/
     $titre = preg_replace("#'|\"#", $replace, htmlspecialchars($_POST['title']));
     $date = preg_replace("#'|\"#", $replace, htmlspecialchars($_POST['date']));
     /*$creationDate*/ /*defini plus haut*/
     $endDate = null;
+    $hour = preg_replace("#'|\"#", $replace, htmlspecialchars($_POST['time']));
     $description = preg_replace("#'|\"#", $replace, html_entity_decode(htmlspecialchars($_POST['description']),ENT_QUOTES));
 
 
     $cout = htmlspecialchars($_POST['quantity']);
-    $nbrParticipants = 0;
+    $nbrParticipants = $DatasEvent['Nbr_participants'];
     /*$url*/ /*defini plus haut*/
-    $nbrVotes = 0;
+    $nbrVotes = $DatasEvent['Nbr_votes'];
     $adresse = preg_replace("#'|\"#", $replace, htmlspecialchars($_POST['address']));
     $idUtilisateur = $_SESSION['id'];
     $idRecurrence = $_POST['recurrence'];
-    $isIdea = 1;
-    $idDate = null;
-    $idAccessibilite = 1;
+    $isIdea = $DatasEvent['Is_idea'];
+    if (isset($_POST['id_status_date']))
+        $idDate = $_POST['id_status_date'];
+    else
+        $idDate = null;
+    $idAccessibilite = $DatasEvent['Id_status_accessibilite'];
 
     /*$recurrence = $local_bdd->query('call orleans_bde.sps_recurrence(\'' . htmlspecialchars($_POST['recurrence']) . '\');');
     $dataRecurrence = $recurrence->fetch();
     $recurrence->closeCursor();*/
 
 
-/*    echo $titre . '<br>';
-    echo $date . '<br>';
-    echo $creationDate . "<br>";
-    echo $endDate . '<br>';
-    echo $description . '<br>';
-    echo $cout . '<br>';
-    echo $nbrParticipants . '<br>';;
-    echo $url . '<br>';
-    echo $nbrVotes . '<br>';
-    echo $adresse . '<br>';
-    echo $idUtilisateur . '<br>';
-    echo $idRecurrence . '<br>';
-    echo $isIdea . '<br>';
-    echo $idDate . '<br>';
-    echo $idAccessibilite . '<br>';*/
+    /*    echo $titre . '<br>';
+        echo $date . '<br>';
+        echo $creationDate . "<br>";
+        echo $endDate . '<br>';
+        echo $description . '<br>';
+        echo $cout . '<br>';
+        echo $nbrParticipants . '<br>';;
+        echo $url . '<br>';
+        echo $nbrVotes . '<br>';
+        echo $adresse . '<br>';
+        echo $idUtilisateur . '<br>';
+        echo $idRecurrence . '<br>';
+        echo $isIdea . '<br>';
+        echo $idDate . '<br>';
+        echo $idAccessibilite . '<br>';*/
 
 
     /*date de debut mise temporairement en attente dune amelioration du formulaire de création*/
-    $query =
-        'call orleans_bde.spe_evenement(
-    ' . $id . ',
+    if (!isset($_POST['id_status_date'])) {
+        $query =
+            'call orleans_bde.spe_evenement(
+    ' . $_SESSION['id_event'] . ',
     \'' . $titre . '\',  
     \'' . $date . '\',  
     \'' . $creationDate . '\',  
     \'' . $date . '\',   
+    \'' . $hour . '\',
     \'' . $description . '\',  
     ' . $cout . ',
     ' . $nbrParticipants . ',
@@ -103,12 +103,41 @@ if ($erreur = 'pas derreur') {
     ' . $idUtilisateur . ',
     ' . $idRecurrence . ',
     ' . $isIdea . ',
+    ' . $idDate . ',
     ' . $idAccessibilite . ');';
 
-/*    echo $query;*/
-/*    echo 'effectué';*/
-/*    echo $erreur;*/
-    $local_bdd->query($query);
+/*            echo $query;*/
+
+        $local_bdd->query($query);
+        echo '<meta http-equiv="refresh" content="0; URL=../idea_box/AccueilBoxIdea.php">';
+    }
+    else {
+        $query =
+            'call orleans_bde.spe_evenement(
+    ' . $_SESSION['id_event'] . ',
+    \'' . $titre . '\',  
+    \'' . $date . '\',  
+    \'' . $creationDate . '\',  
+    \'' . $date . '\',   
+    \'' . $hour . '\',
+    \'' . $description . '\',  
+    ' . $cout . ',
+    ' . $nbrParticipants . ',
+    \'' . $url . '\',  
+    ' . $nbrVotes . ',
+    \'' . $adresse . '\',  
+    ' . $idUtilisateur . ',
+    ' . $idRecurrence . ',
+    ' . $isIdea . ',
+    ' . $idDate . ',
+    ' . $idAccessibilite . ');';
+        /*    echo $query;*/
+
+        $local_bdd->query($query);
+        echo '<meta http-equiv="refresh" content="0; URL=../events_to_come/index.php">';
+    }
+
+
 }
 else {
     echo '<p> Une erreur a été rencontrée lors de l\'upload du fichier </p>';
